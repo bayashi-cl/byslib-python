5eac78a876866f87f9838bfe995922f18692cce1
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-02 18:00:32 +0900

    テンプレ追加

---
+++ template.py
@@ -0,0 +1,21 @@
+# region template
+import sys
+import typing
+from typing import Callable, Dict, List, Set, Tuple
+
+sys.setrecursionlimit(10 ** 6)
+Vec = List[int]
+VecVec = List[Vec]
+sinput: Callable[..., str] = sys.stdin.readline
+MOD: int = 998244353
+INF: float = float("Inf")
+IINF: int = sys.maxsize // 2
+# endregion
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    main()
